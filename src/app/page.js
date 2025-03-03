fde3308bb90dac97dc78561e5363896e808e3007
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-03-03 22:06:02 +0300

    add styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,66 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+
+function TrafficChange(props) {
+
+  //массив состояний цветов светофора
+  let [states, setState] = useState(["red", "gray", "gray"])
+ //функция изменения цветов
+  function changeState()
+  {
+    if(states[0]=="red"){
+    setState(["gray", "yellow", "gray"])
+    }
+    if(states[1]=="yellow"){
+    setState(["gray", "gray", "green"])
+    }
+    if(states[2]=="green"){
+    setState(["red", "gray", "gray"])
+    }
+  }
+
+//стили
+ let trafficLight = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    width: "120px",
+    height: "350px",
+    backgroundColor: "lightgray"
+  };
+ let light = {
+    width: "100px",
+    height: "100px",
+    border: "1px solid",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: "100px"
+  };
+ let redLight = {
+   backgroundColor: states[0]
+  }
+ let yellowLight = {
+  backgroundColor: states[1]
+  }
+let greenLight = {
+  backgroundColor: states[2]
+  }
+
+  return(
+<div style={trafficLight}>
+        <div style={{...light, ...redLight}} onClick={changeState}></div>
+        <div style={{...light, ...yellowLight}} onClick={changeState}></div>
+        <div style={{...light, ...greenLight}} onClick={changeState}></div>
+      </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     <TrafficChange/>
     </div>
   );
 }
